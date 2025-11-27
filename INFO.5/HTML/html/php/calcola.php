e525d9eda816551d30cc0d0e3fec60db09ce8c42
Momo74C <150558954+Momo74C@users.noreply.github.com>
2025-11-27 10:31:30 +0000

    update

---
+++ INFO.5/HTML/html/php/calcola.php
@@ -0,0 +1,35 @@
+<?php
+
+$R = $_POST['primonumero'];
+$S = $_POST['secondonumero'];
+
+$op = $_POST['operazione'];
+
+switch ($op) {
+	case 'somma':
+		$res = $R + $S;
+		echo "La somma di $R e $S è uguale a $res";
+		break;
+	case 'differenza':
+		$res = $R - $S;
+		echo "La differenza di $R e $S è uguale a $res";
+		break;
+	case 'prodotto':
+		$res = $R * $S;
+		echo "Il prodotto di $R e $S è uguale a $res";
+		break;
+	case 'divisione':
+		if ($S == 0) {
+			echo "Errore: divisione per zero.";
+		} else {
+			$res = $R / $S;
+			echo "La divisione di $R per $S è uguale a $res";
+		}
+		break;
+	default:
+		echo "Operazione non valida.";
+}
+
+?>
+
+?>
--- INFO.5/HTML/html/php/somma.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$R = $_POST['primonumero'];
-$S = $_POST['secondonumero'];
-$Somma = $R + $S;
-echo "La somma di $R e $S è uguale a $Somma";
-?>
