1356c07af818aa3260f7e70cc60e9015355e74df
Momo74C <150558954+Momo74C@users.noreply.github.com>
2025-12-04 09:59:13 +0000

    update

---
+++ PHP/conn.php
@@ -0,0 +1,9 @@
+<?php
+$conn = new mysqli("localhost", "root", "", "prova");
+
+if ($conn->connect_error) {
+    die("Connessione fallita: " . $conn->connect_error);
+}
+
+echo "Connessione riuscita!";
+?>
