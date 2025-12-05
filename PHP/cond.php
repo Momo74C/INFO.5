1357c88c6443f68b6c8eb507e4e556f91ebeb5cb
Momo74C <150558954+Momo74C@users.noreply.github.com>
2025-12-05 12:08:17 +0000

    update

---
+++ PHP/cond.php
@@ -0,0 +1,35 @@
+<?php
+$conn = new mysqli("localhost", "root", "", "progetto_scuola");
+
+if ($conn->connect_error) {
+    die("Connessione fallita: " . $conn->connect_error);
+}
+
+$co = $_POST['condizione'];
+
+switch ($co) {
+    case 'studenti con voto sufficente':
+        $sql = "SELECT * FROM studenti WHERE voto >= 6";
+        break;
+    case 'studenti con voto pari a 10':
+        $sql = "SELECT * FROM studenti WHERE voto = 10";
+        break;
+    case 'Cognome simili':
+        $sql = "SELECT * FROM studenti WHERE cognome LIKE 'elshourbgy'";
+        break;
+    default:
+        echo "Condizione non valida.";
+}
+
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    while($row = $result->fetch_assoc()) {
+        echo "Nome: " . $row["nome"]. " - Cognome: " . $row["cognome"]. " - Voto: " . $row["voto"]. "<br>";
+    }
+} else {
+    echo "Nessun risultato trovato.";
+}
+
+$conn->close();
+?>
